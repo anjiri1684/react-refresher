9d2eb0866e986a130e967ba052ed8dc7480f7a08
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-19 12:32:02 +0300

    "Added useEffect hook to wrap meetup data fetching and removed immediate function call"

--- practices101/src/pages/AllMeetups.js
+++ practices101/src/pages/AllMeetups.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import MeetupList from "../components/meetups/MeetupList";
 
@@ -6,14 +6,16 @@ function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch("https://react-practices101-default-rtdb.firebaseio.com/meetups.json")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
+  useEffect(() => {
+    fetch("https://react-practices101-default-rtdb.firebaseio.com/meetups.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        setLoadedMeetups(data);
+      });
+  }, []);
 
   if (isLoading) {
     return (
