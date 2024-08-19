d8f18c6d25d15aca3195980393073f69b6352222
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-19 13:11:49 +0300

    "Wrapped App component with FavoritesContextProvider, implemented context provider with state and functions for managing favorites"

--- practices101/src/store/favorites-context.js
+++ practices101/src/store/favorites-context.js
@@ -1,13 +1,41 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const FavoritesContext = createContext({
   favorites: [],
   totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {},
 });
 
-function FavoritesContextProvider({ children }) {
-  const context = {};
+export function FavoritesContextProvider({ children }) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
+  };
   return (
     <FavoritesContext.Provider value={context}>
       {children}
@@ -15,4 +43,4 @@ function FavoritesContextProvider({ children }) {
   );
 }
 
-export default FavoritesContextProvider;
+export default FavoritesContext;
